12c1dccf7e0c03308c135da29ed6d0d7975c8482
Osiris8 <miganosiris8@gmaaail.com>
2025-06-15 00:40:52 +0200

    Update calculate.py file

--- calculate.py
+++ calculate.py
@@ -1,1 +1,54 @@
-print(25 * 68 + 13 / 28)
+# First we create the variables
+quilt_width = 8
+quilt_length = 12
+
+# Then we print the size
+print(quilt_width * quilt_length)
+
+# Whoops! That's a little too large
+quilt_length = 8
+
+# Let's see how large it is now
+print(quilt_width * quilt_length)
+
+# 2 to the 10th power, or 1024
+print(2 ** 10)
+
+# 8 squared, or 64
+print(8 ** 2)
+
+# 9 * 9 * 9, 9 cubed, or 729
+print(9 ** 3)
+
+# We can even perform fractional exponents
+# 4 to the half power, or 2
+print(4 ** 0.5)
+
+
+#Modulo
+first_order_remainder = 269 % 10
+print(first_order_remainder)
+
+first_order_coupon = "no"
+
+second_order_remainder = 270 % 10
+print(second_order_remainder)
+
+second_order_coupon = "yes"
+
+#Concatenation
+message = "Hello"
+world = "World"
+
+print(message + world)
+
+number_of_freelance = 10
+number_of_freelance+= 2
+print(number_of_freelance)
+
+#Multi-line String
+to_you = """Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you?"""
+
+print(to_you)
