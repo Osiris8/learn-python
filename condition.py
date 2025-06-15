48e8760fcae2526ff256d71b394749c61f7ad3e1
Osiris8 <miganosiris8@gmaaail.com>
2025-06-15 10:13:13 +0200

    learn python condition

---
+++ condition.py
@@ -0,0 +1,23 @@
+user_name = "Dave"
+
+if user_name == "Dave":
+    print("Get off my computer, Dave!")
+
+user_name = "Johnson"
+if user_name != "Dave":
+    print("Get on my computer")
+
+x = 20
+y = 20
+
+if x == y:
+    print("These numbers are the same")
+
+
+name_of_ai = "chatgpt"
+if name_of_ai == "claude":
+        print("It's not chatgpt")
+elif name_of_ai == "perplexity":
+     print("it's not chatgptai")
+else:
+     print("Hello World")
