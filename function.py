4c7c7fd10f0b7d61588f3a16d8a8d2e454bcd46e
Osiris8 <miganosiris8@gmaaail.com>
2025-06-15 11:31:06 +0200

    learn python function

---
+++ function.py
@@ -0,0 +1,13 @@
+def name_of_ai(ai):
+    print("The name of this AI is" + ai)
+
+name_of_ai("chatgpt")
+
+
+def my_function(x):
+  return x + 1
+
+
+
+print(my_function(2))      
+print(my_function(3 + 5)) 
