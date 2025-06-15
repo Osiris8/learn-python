e99e745e1a16e6502bc9de41501688c88c37ed75
Osiris8 <miganosiris8@gmaaail.com>
2025-06-15 10:56:34 +0200

    learn python list

---
+++ list.py
@@ -0,0 +1,16 @@
+primes = [1, 2, 3, 4, 5]
+print(primes)
+
+total_primes = primes + [6,  7, "Hello"]
+print(total_primes)
+
+another_list = [["red", 1], ["yellow", 2]]
+print(another_list)
+
+
+class_name_hobbies = [["Jenny", "Breakdancing"], ["Alexus", "Photography"], ["Grace", "Soccer"]]
+
+
+class_name_hobbies[0][1] = "Meditation"
+print(class_name_hobbies)
+
